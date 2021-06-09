b1355c321e7df4d4d0d213c800d2b57b6f5edc66
CaduGomes <caduvitorinog@gmail.com>
2021-06-09 14:41:33 -0300

    Initial commit

---
+++ lib/presentation/pages/bluetooth-devices/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BluetoothDevices extends StatefulWidget {
+  const BluetoothDevices({Key? key}) : super(key: key);
+
+  @override
+  _BluetoothDevicesState createState() => _BluetoothDevicesState();
+}
+
+class _BluetoothDevicesState extends State<BluetoothDevices> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
